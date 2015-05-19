630fdfc158e18165965bcff8345247f718512a92
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-05-18 22:05:59 -0500

    Add a controller and some simple data

---
+++ app/assets/javascript/controllers/ProductListController.js
@@ -0,0 +1,28 @@
+app.controller('ProductListController', function() {
+  this.products = [
+    {
+      id: 1,
+      company: 2,
+      name: 'Foo Wax',
+      description: 'Lorem Foo Wax dolor sit amet, consectetur adipisicing elit. Perspiciatis ullam consequuntur velit quibusdam non laboriosam porro Foo eos provident ab eum, wax Foo, rem unde quos sapiente natus eaque voluptatibus!'
+    },
+    {
+      id: 2,
+      company: 1,
+      name: 'Bacon Product',
+      description: 'Bacon ipsum dolor amet beef shank alcatra pork belly sausage kielbasa hamburger pastrami spare ribs pig kevin. Shoulder hamburger biltong, jowl tenderloin doner shankle andouille salami meatball drumstick frankfurter tongue t-bone. Capicola venison cupim, kevin picanha ribeye sausage tongue beef ribs bacon alcatra jerky rump shankle.'
+    },
+    {
+      id: 3,
+      company: 4,
+      name: 'Italian Prochetta',
+      description: 'Porchetta ball tip pastrami swine cupim meatloaf chuck sirloin meatball pancetta fatback. Boudin brisket sirloin jowl strip steak fatback turkey doner.'
+    },
+    {
+      id: 4,
+      company: 18,
+      name: 'Pork Chop',
+      description: 'Deserunt eu id pork chop nisi consequat in commodo flank pork. Doner fugiat corned beef jerky, filet mignon ground round ut short ribs meatloaf nisi.'
+    }
+  ];
+});
