5926ad8990f2495a4f1b50349bb540e1457c4c53
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-05-26 21:25:25 -0500

    Add delete function to product show page

--- app/assets/javascript/controllers/ProductShowController.js
+++ app/assets/javascript/controllers/ProductShowController.js
@@ -1,3 +1,12 @@
-app.controller('ProductShowController', function(Product, $scope, $routeParams) {
+app.controller('ProductShowController', function(Product, $scope, $routeParams, $location) {
   $scope.product = Product.get({productId: $routeParams.id});
+
+  $scope.deleteProduct = function(product) {
+    if (window.confirm('Are you sure you want to delete "' + product.name + '"? ' +
+                      'This action cannot be undone.')) {
+      product.$delete().finally(function() {
+        $location.path('/products/');
+      });
+    }
+  };
 });
