c1a83e68a97eb797d81e900271262840631b5467
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-05-27 16:00:13 -0500

    Add shutdown notification and process title

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+process.title = 'Feedback-App';
+
 var app = require('./server/routes');
 
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
+});
+
+/**
+ * Shutdown the server process
+ *
+ * @param {String}  signal    Signal to send, such as 'SIGINT' or 'SIGHUP'
+ */
+var close = function(signal) {
+  console.log('Server shutting down');
+  process.kill(process.pid, signal);
+};
+
+process.stdin.resume();
+
+// Attach event listeners
+process.on('SIGINT', function() {
+  close('SIGINT');
+});
+
+process.on('SIGHUP', function() {
+  close('SIGHUP');
+});
+
+process.on('SIGTERM', function() {
+  close('SIGTERM');
 });
