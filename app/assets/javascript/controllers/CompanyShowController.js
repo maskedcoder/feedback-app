59e2e8b24a1a275c5dc6402b2167371219a582aa
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-06-03 18:17:37 -0500

    Add average rating display; warn for no reviews

--- app/assets/javascript/controllers/CompanyShowController.js
+++ app/assets/javascript/controllers/CompanyShowController.js
@@ -1,6 +1,21 @@
 app.controller('CompanyShowController', function(Company, $scope, $routeParams, $location) {
   $scope.company = Company.get({companyId: $routeParams.id});
 
+  $scope.stars = function() {
+    var starCount = 0;
+
+    // This may get called before the company data is fetched
+    if (!$scope.company.reviews) {
+      return 0;
+    }
+
+    $scope.company.reviews.forEach(function(review) {
+      starCount += review.stars;
+    });
+
+    return starCount / $scope.company.reviews.length;
+  };
+
   $scope.deleteCompany = function(company) {
     if (window.confirm('Are you sure you want to delete "' + company.name + '"? ' +
                       'This action cannot be undone.')) {
