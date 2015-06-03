b3b5aa1b8f8d60ecdc19431aef1cf1cd45d86b8f
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-06-03 17:03:33 -0500

    Add Company Review data

--- server/models.js
+++ server/models.js
@@ -17,6 +17,49 @@ exports.companyData = [
   }
 ];
 
+exports.companyReviewData = [
+  {
+    id: 1,
+    stars: 3,
+    company: 2,
+    name: 'Algernon Skevich',
+    title: 'Provident Ab Eum',
+    description: 'Lorem dolor sit amet, consectetur adipisicing elit. Perspiciatis ullam consequuntur velit quibusdam non laboriosam porro Foo eos provident ab eum, wax Foo, rem unde quos sapiente natus eaque voluptatibus!'
+  },
+  {
+    id: 2,
+    stars: 2,
+    company: 1,
+    name: 'Nunc Provident',
+    title: 'Lorem ipsum dolor',
+    description: 'Morbi sollicitudin, elit ac dignissim pretium, tellus velit eleifend ligula, sed pulvinar neque ante ac justo. Nunc accumsan cursus odio, vel ultricies magna accumsan ut. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    id: 3,
+    stars: 5,
+    company: 2,
+    name: 'Quisque Donec',
+    title: 'lacus metus',
+    description: 'Pellentesque lacus metus, faucibus at finibus in, lobortis viverra augue. Morbi sodales quam sed sem malesuada pulvinar.'
+  },
+  {
+    id: 4,
+    stars: 5,
+    company: 3,
+    name: 'Phasellus Rex',
+    title: 'Nunc turpis Risus',
+    description: 'Vestibulum eget augue quis neque ullamcorper gravida at et velit. Donec porta, dolor sed rhoncus blandit, odio risus maximus velit, sit amet suscipit turpis augue in mi. Duis condimentum nibh est, ac sollicitudin metus fringilla sed. Nulla suscipit tellus id neque hendrerit tincidunt. Vestibulum finibus, nibh id tempus egestas, nisi urna laoreet erat, at convallis lectus nulla vitae justo. Nunc turpis risus, viverra id accumsan non, sollicitudin eu nulla. Morbi ullamcorper neque eu leo porttitor, non facilisis tortor lacinia.'
+  },
+  {
+    id: 5,
+    stars: 4,
+    company: 2,
+    name: 'Suspendisse',
+    title: 'Pharetra enim posuere',
+    description: 'Aenean cursus dolor at purus tempor, nec gravida augue faucibus. Aliquam eget tortor ante. Vivamus malesuada purus et tellus luctus, eget pharetra enim posuere. Donec nec urna justo. Suspendisse potenti. Donec a vestibulum neque, a fermentum massa. Cras quis posuere eros. Fusce rhoncus id nisl et bibendum. Sed dolor diam, gravida ut congue nec, accumsan nec felis. Vivamus pharetra justo in nunc consequat consequat.'
+  }
+];
+
 exports.productData = [
   {
     id: 1,
